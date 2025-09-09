8d19991a0802f6a20591d5e0d216a4cf3cb28de3
dov zucker <ydovzucker@gmail.com>
2025-09-09 10:09:52 +0300

    changed library for getting meta data and hashed id

--- manager.py
+++ manager.py
@@ -4,12 +4,20 @@ from meta_data_files import get_meta_data,convert_meta_data_to_json
 from publisher.publisher import Publisher
 from consumer.consumer import consume_meta_data
 from elastic.main import index_json_to_elastic
-import json
+
+import hashlib
 
 
 import json
 
 from consumer.consumer import consume_meta_data
+def hash_for_file_name(file_name):
+
+    encoded_string = file_name.encode('utf-8')
+    hash_func = hashlib.sha256(encoded_string).hexdigest()[:6]
+    return hash_func
+
+
 def main():
     topic = "meta_data_for_audio_file"
     messages = consume_meta_data(topic)
--- meta_data_files.py
+++ meta_data_files.py
@@ -1,78 +1,44 @@
-from tinytag import TinyTag
+import hashlib
 import json
-import wave
+
+import os
 from pathlib import Path
 
 
-import os
-
-# def get_all_file_paths(folder_path):
-#     """
-#     Loops over a folder and its subfolders to get the full path of every file.
-#
-#     Args:
-#         folder_path (str): The path to the folder to start the search from.
-#
-#     Returns:
-#         list: A list containing the full paths of all files found.
-#     """
-#     all_file_paths = []
-#     for root, _, files in os.walk(folder_path):
-#         for file in files:
-#             full_path = os.path.join(root, file)
-#             meta_data = TinyTag.get(full_path)
-#
-#             all_file_paths.append()
-#     return all_file_paths
 def get_meta_data(full_path):
-    tag = TinyTag.get(full_path)
-    return tag
-def convert_meta_data_to_json(tag,full_path):
-    data = {
-        "file_path": full_path,
-        "metadata": {
-            "title": tag.title,
-            "artist": tag.artist,
-            "album": tag.album,
-            "duration": tag.duration,  # in seconds
-            "filesize": tag.filesize,  # in bytes
-            # Add other metadata fields as needed
-        }
-    }
-    json_string = json.dumps(data, indent=4)
 
+    file_path = Path(full_path)
+    try:
+        stats = file_path.stat()
+        return stats
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+
+def convert_meta_data_to_json(stats,full_path):
+    meta_data = {
+        "file_path": full_path,
+        f"File size": f"{stats.st_size} bytes",
+        "Last modified time": os.path.getmtime(full_path),
+        "Creation time": os.path.getctime(full_path),
+        f"Permissions": stats.st_mode,
+        f"Inode number": stats.st_ino,
+        "file name": os.path.basename(full_path)
+
+    }
+
+
+    json_string = json.dumps(meta_data, indent=4)
     return json_string
+#
 
 # Example usage
 if __name__ == "__main__":
-    # target_folder = 'C:/audio-files'  # Replace with the actual folder path
-    # file_paths = get_all_file_paths(target_folder)
 
-    # for path in file_paths:
-    #     print(path)
     meta_data = get_meta_data(r"C:/audio-files\download (6).wav")
     print(convert_meta_data_to_json(meta_data,r"C:/audio-files\download (6).wav"))
 
 
 
-
-
-
-# import wave
-# from pathlib import Path
-#
-#     wav_file_path = Path("your_audio_file.wav") # Replace with your WAV file path
-#
-#     try:
-#         with wave.open(str(wav_file_path), 'rb') as wf:
-#             print(f"Number of channels: {wf.getnchannels()}")
-#             print(f"Sample width (bytes): {wf.getsampwidth()}")
-#             print(f"Frame rate (sample rate): {wf.getframerate()}")
-#             print(f"Number of frames: {wf.getnframes()}")
-#             print(f"Compression type: {wf.getcomptype()}")
-#             print(f"Compression name: {wf.getcompname()}")
-#     except wave.Error as e:
-#         print(f"Error reading WAV file: {e}")
 
 
 
